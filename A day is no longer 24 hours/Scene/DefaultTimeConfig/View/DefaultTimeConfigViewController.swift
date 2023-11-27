14f23b7b22b175dd74b0e8613182b40108586ed8
SeungwooSeo <prodvssw.98@gmail.com>
2023-11-28 00:08:00 +0900

    [Refactor] DefaultTimeConfig Flow 리팩토링
    Rx
    Input Output 패턴
    MVVM

--- A day is no longer 24 hours/Scene/DefaultTimeConfig/View/DefaultTimeConfigViewController.swift
+++ A day is no longer 24 hours/Scene/DefaultTimeConfig/View/DefaultTimeConfigViewController.swift
@@ -5,113 +5,77 @@
 //  Created by 서승우 on 2023/10/12.
 //
 
-import HGCircularSlider
 import UIKit
+import HGCircularSlider
+import RxCocoa
+import RxSwift
 
 final class DefaultTimeConfigViewController: BaseViewController {
     // MARK: - View
     private let mainView = DefaultTimeConfigMainView()
 
-    // MARK: - ViewModel
-    private let viewModel: DefaultTimeConfigViewModel
+    private let disposeBag = DisposeBag()
 
-    // MARK: - Init
-    private init(_ viewModel: DefaultTimeConfigViewModel) {
-        self.viewModel = viewModel
+    init(viewModel: DefaultTimeConfigViewModel) {
         super.init(nibName: nil, bundle: nil)
-    }
 
-    convenience init(viewModel: DefaultTimeConfigViewModel) {
-        self.init(viewModel)
+        let pointValueChanged = PublishRelay<(start: CGFloat, end: CGFloat)>()
 
-        viewModel.whenIsBedTimeToString.bind { [weak self] (text) in
-            guard let self else {return}
-            self.mainView.bedTimeLabel.text = text
-        }
+        let input = DefaultTimeConfigViewModel.Input(
+            sliderValueChanged: mainView.rangeCircularSlider.rx.controlEvent(.valueChanged),
+            pointValueChanged: pointValueChanged,
+            nextButtonTapped: mainView.nextButtom.rx.tap
+        )
 
-        viewModel.whenIsWakeUpTimeToString.bind { [weak self] (text) in
-            guard let self else {return}
-            self.mainView.wakeUpTimeLabel.text = text
-        }
-
-        viewModel.howMuchSleepTimeToString.bind { [weak self] (howMuchSleepTimeToString) in
-            guard let self else {return}
-            self.mainView.sleepHourAndMinuteLabel.text = howMuchSleepTimeToString
-        }
-
-        viewModel.howMuchSleepTimeValidity.bind { [weak self] (bool) in
-            guard let self else {return}
-            if bool {
-                self.mainView.nextButtom.isEnabled = true
-                self.mainView.rangeCircularSlider.trackFillColor = Constraints.Color.white
-                self.mainView.sleepHourToMinuteValidityLabel.textColor = Constraints.Color.white
-                self.mainView.sleepHourToMinuteValidityLabel.text = "적절한 수면 시간을 정해보세요!"
-            } else {
-                self.mainView.nextButtom.isEnabled = false
-                self.mainView.rangeCircularSlider.trackFillColor = Constraints.Color.red
-                self.mainView.sleepHourToMinuteValidityLabel.textColor = Constraints.Color.red
-                self.mainView.sleepHourToMinuteValidityLabel.text = "수면 시간이 너무 작거나 너무 큽니다."
+        let output = viewModel.transform(input: input)
+        output.adjustValue
+            .bind(with: self) { owner, _ in
+                // Rx스럽진 않지만 inout 파라미터를 사용해야하기 때문에 어쩔 수 없다.
+                viewModel.adjustValue(value: &owner.mainView.rangeCircularSlider.startPointValue)
+                viewModel.adjustValue(value: &owner.mainView.rangeCircularSlider.endPointValue)
+                pointValueChanged.accept(
+                    (owner.mainView.rangeCircularSlider.startPointValue,
+                     owner.mainView.rangeCircularSlider.endPointValue)
+                )
             }
-        }
-    }
+            .disposed(by: disposeBag)
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        output.whenIsBedTimeToString
+            .bind(to: mainView.bedTimeLabel.rx.text)
+            .disposed(by: disposeBag)
+
+        output.whenIsWakeUpTimeToString
+            .bind(to: mainView.wakeUpTimeLabel.rx.text)
+            .disposed(by: disposeBag)
+
+        output.howMuchSleepTimeToString
+            .bind(to: mainView.sleepHourAndMinuteLabel.rx.text)
+            .disposed(by: disposeBag)
+
+        output.howMuchSleepTimeState
+            .bind(with: self) { owner, state in
+                switch state {
+                case .available:
+                    owner.mainView.nextButtom.isEnabled = true
+                    owner.mainView.rangeCircularSlider.trackFillColor = Constraints.Color.white
+                    owner.mainView.sleepHourToMinuteValidityLabel.textColor = Constraints.Color.white
+                case .unavailable:
+                    owner.mainView.nextButtom.isEnabled = false
+                    owner.mainView.rangeCircularSlider.trackFillColor = Constraints.Color.red
+                    owner.mainView.sleepHourToMinuteValidityLabel.textColor = Constraints.Color.red
+                }
+                owner.mainView.sleepHourToMinuteValidityLabel.text = state.description
+            }
+            .disposed(by: disposeBag)
     }
 
     override func loadView() {
         view = mainView
     }
 
-    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
-    // MARK: - Initial Setting
-    override func initialAttributes() {
-        super.initialAttributes()
-
-        view.backgroundColor = Constraints.Color.black
-        mainView.nextButtom.addTarget(
-            self,
-            action: #selector(didTapNextButton),
-            for: .touchUpInside
-        )
-        mainView.rangeCircularSlider.addTarget(
-            self,
-            action: #selector(valueChangedRangeCircularSlider),
-            for: .valueChanged
-        )
-
-    }
-
-    // MARK: - Event
-    @objc
-    private func didTapNextButton(
-        _ sender: UIButton
-    ) {
-        // 애초에 nextButton은 활성화가 되어 있어야 tap
-        viewModel.nextButtonTapped.value.toggle()
-    }
-
-    @objc
-    private func valueChangedRangeCircularSlider(
-        _ sender: RangeCircularSlider
-    ) {
-        // TODO: 비즈니스 로직들인데 여기서 직접 사용하는게 맘에 안든다. 근데 메서드 따로 만들어서 했는데 왜 안되는지 모르겠음 - 1
-        viewModel.adjustValue(
-            value: &sender.startPointValue
-        )
-        viewModel.adjustValue(
-            value: &sender.endPointValue
-        )
-        viewModel.updateTimes(
-            startPointValue: sender.startPointValue,
-            endPointValue: sender.endPointValue
-        )
-        viewModel.updateHowMuchSleepTimeValidity()
-    }
-
+    
 }
